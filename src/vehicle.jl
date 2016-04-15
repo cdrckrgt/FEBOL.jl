26172fdefb3b10a872088452284dcb71a2532da3
Louis Dressel <dressel@stanford.edu>
2016-04-15 07:37:25 -0700

    constrain vehicle heading to be within [0,360)

--- src/vehicle.jl
+++ src/vehicle.jl
@@ -14,27 +14,32 @@ type Vehicle
 	max_step::Float64
 	sensor::Sensor
 
-	function Vehicle(x::Real,y::Real,h::Real,ms::Real,s::Sensor)
+	function Vehicle(x::Real, y::Real, h::Real, ms::Real, s::Sensor)
 		return new(float(x),float(y),float(h),float(ms),s)
 	end
-	#Vehicle(x::Real,y::Real,ns::Real) = new(float(x),float(y),float(ns),2.0)
 
+	# Default uses BearingOnly
 	function Vehicle(x::Real, y::Real)
-		#return new( float(x), float(y), 0.0, 2.0, BearingOnly(10.0) )
-		return new( float(x), float(y), 0.0, 2.0, DirOmni("norm360.csv") )
+		return new( float(x), float(y), 0.0, 2.0, BearingOnly(10.0) )
 	end
+	function Vehicle(x::Real, y::Real, s::Sensor)
+		return new( float(x), float(y), 0.0, 2.0, s)
+	end
+
 end
 
+# Does the bounds checking on the action, to see if stays in search domain
+# Ensures heading is within [0,360) (other parts of code assume this)
 function new_location(m::SearchDomain, x::Vehicle, a::Action)
 	new_x = max(min(x.x + a[1], m.length), 0.0)
 	new_y = max(min(x.y + a[2], m.length), 0.0)
-	return new_x, new_y
+	new_h = mod(x.heading + a[3], 360.0)
+	return new_x, new_y, new_h
 end
 
 """
 `act!(m::SearchDomain, x::Vehicle, a::Action)`
 """
 function act!(m::SearchDomain, x::Vehicle, a::Action)
-	x.x, x.y = new_location(m, x, a)
-	x.heading += a[3]
+	x.x, x.y, x.heading = new_location(m, x, a)
 end
