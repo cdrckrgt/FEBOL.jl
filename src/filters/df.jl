e91bcfe1ae6c92430708dad6d8c06053f10eccae
Louis Dressel <dressel@stanford.edu>
2016-07-06 07:21:20 -0700

    making discrete filter's covariance better; adding centroid/covariance for matrices

--- src/filters/df.jl
+++ src/filters/df.jl
@@ -51,6 +51,7 @@ end
 
 
 # returns x, y value
+# Assumes that the belief sums to one
 function centroid(df::DF)
 	x_val = 0.0; y_val = 0.0
 	for x = 1:df.n
@@ -62,21 +63,67 @@ function centroid(df::DF)
 	return x_val*df.cell_size, y_val*df.cell_size
 end
 
+function centroid(d::Matrix{Float64}, L::Float64)
+	x_val = 0.0; y_val = 0.0
+	n = size(d,1)
+	cell_size = L / n
+	d_sum = 0.0
+	for x = 1:n
+		for y = 1:n
+			x_val += (x-.5) * d[x,y]
+			y_val += (y-.5) * d[x,y]
+			d_sum += d[x,y]
+		end
+	end
+	return x_val*cell_size/d_sum, y_val*cell_size/d_sum
+end
+
 function covariance(df::DF)
-	mu_x, mu_y = centroid(df)
+	mu_x, mu_y = 0.0
 	c_xx = c_xy = c_yy = 0.0
 	for xi = 1:df.n
 		for yi = 1:df.n
 			x = (xi-0.5)*df.cell_size
 			y = (yi-0.5)*df.cell_size
 
+			mu_x += x * df.b[xi,yi]
+			mu_y += y * df.b[xi,yi]
+
 			c_xx += df.b[xi,yi] * x * x
 			c_yy += df.b[xi,yi] * y * y
-			c_xy += df.b[xi,yi] * (x - mu_x) * (y - mu_y)
+			c_xy += df.b[xi,yi] * x * y
 		end
 	end
 	c_xx -= (mu_x * mu_x)
 	c_yy -= (mu_y * mu_y)
+	c_xy -= (mu_x * mu_y)
+	return [c_xx+1e-4 c_xy; c_xy c_yy+1e-4]
+end
+
+
+# add some to ensure we are 
+function covariance(d::Matrix{Float64}, L::Float64)
+	mu_x, mu_y = centroid(d, L)
+	c_xx = c_xy = c_yy = 0.0
+	n = size(d,1)
+	cell_size = L / n
+	d_sum = 0.0
+	for xi = 1:n
+		for yi = 1:n
+			x = (xi-0.5)*cell_size
+			y = (yi-0.5)*cell_size
+
+			c_xx += d[xi,yi] * x * x
+			c_yy += d[xi,yi] * y * y
+			c_xy += d[xi,yi] * (x - mu_x) * (y - mu_y)
+			d_sum += d[xi,yi]
+		end
+	end
+	c_xx = c_xx/d_sum - (mu_x * mu_x)
+	c_yy = c_yy/d_sum - (mu_y * mu_y)
+	c_xy = c_xy/d_sum
+
+	# add 1e-3 to ensure we are positive definite
 	return [c_xx+1e-3 c_xy; c_xy c_yy+1e-3]
 end
 
