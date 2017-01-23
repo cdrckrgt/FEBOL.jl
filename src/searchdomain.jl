68083d74c9585f072cbcc7763a883e2d3b85c439
Louis Dressel <dressel@stanford.edu>
2017-01-23 12:27:59 -0800

    added theta! method

--- src/searchdomain.jl
+++ src/searchdomain.jl
@@ -2,6 +2,19 @@
 # searchdomain.jl
 # Handles the type and functions required for the search domain.kl;w
 ######################################################################
+"""
+type `SearchDomain`
+
+Constructors:
+
+`SearchDomain(length, theta_x, theta_y)`
+
+Makes a square search domain with a side-length of `length`. The RF source is located at `(theta_x, theta_y)`.
+
+`SearchDomain(length)`
+
+Assumes a random location to the RF source (theta).
+"""
 type SearchDomain
 	length::Float64
 	theta::LocTuple
@@ -27,8 +40,14 @@ end
 
 `theta!(m::SearchDomain, theta::LocTuple)`
 
+`theta!(m::SearchDomain)`
+
 Change the jammer location of the search domain.
+
+If no location is provided, a random one is selected.
 """
+theta!(m::SearchDomain) = theta!(m, m.length * rand(), m.length * rand())
+
 function theta!(m::SearchDomain, xj::Real, yj::Real)
 	theta!( m, (float(xj), float(yj)) )
 end
