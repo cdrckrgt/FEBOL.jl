ace6090aac1ec175beed1f007550be2e8848b289
Louis Dressel <dressel@stanford.edu>
2017-01-23 10:22:12 -0800

    improving action generation in greedy policies

--- src/policies/greedy.jl
+++ src/policies/greedy.jl
@@ -28,46 +28,35 @@ type GreedyPolicy <: Policy
 	end
 
 	function GreedyPolicy(x::Vehicle, ::DirOmni, n::Int)
-
-		angles = linspace(0.0, 360 - 360/n, n)
-
-		# create list of actions
-		actions = Array(Action, 3n+3)
-		for i = 1:n
-			ax = x.max_step * sind(angles[i])
-			ay = x.max_step * cosd(angles[i])
-
-			actions[i] = (ax, ay, -10.0)
-			actions[i+n] = (ax, ay, 0.0)
-			actions[i+2n] = (ax, ay, 10.0)
-		end
-		actions[3n+1] = (0.0, 0.0, -10.0)
-		actions[3n+2] = (0.0, 0.0, 0.0)
-		actions[3n+3] = (0.0, 0.0, 10.0)
-
+		actions = make_action_list(x, n)
 		return new(n, actions)
 	end
 
 	function GreedyPolicy(x::Vehicle, ::FOV, n::Int)
-
-		angles = linspace(0.0, 360 - 360/n, n)
-
-		# create list of actions
-		actions = Array(Action, 3n+3)
-		for i = 1:n
-			ax = x.max_step * sind(angles[i])
-			ay = x.max_step * cosd(angles[i])
-
-			actions[i] = (ax, ay, -10.0)
-			actions[i+n] = (ax, ay, 0.0)
-			actions[i+2n] = (ax, ay, 10.0)
-		end
-		actions[3n+1] = (0.0, 0.0, -10.0)
-		actions[3n+2] = (0.0, 0.0, 0.0)
-		actions[3n+3] = (0.0, 0.0, 10.0)
-
+		actions = make_action_list(x, n)
 		return new(n, actions)
 	end
+end
+
+function make_action_list(x::Vehicle, n::Int)
+	angles = linspace(0.0, 360 - 360/n, n)
+	d_angle = 10.0
+
+	# create list of actions
+	actions = Array(Action, 3n+3)
+	for i = 1:n
+		ax = x.max_step * sind(angles[i])
+		ay = x.max_step * cosd(angles[i])
+
+		actions[i] = (ax, ay, -d_angle)
+		actions[i+n] = (ax, ay, 0.0)
+		actions[i+2n] = (ax, ay, d_angle)
+	end
+	actions[3n+1] = (0.0, 0.0, -d_angle)
+	actions[3n+2] = (0.0, 0.0, 0.0)
+	actions[3n+3] = (0.0, 0.0, d_angle)
+
+	return actions
 end
 
 
