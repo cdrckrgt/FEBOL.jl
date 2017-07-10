29f34b45e162594559c4aeac30bec417a25f37de
Louis Dressel <dressel@stanford.edu>
2017-07-10 11:31:02 -0700

    added a second movemnt cost

--- src/simulations/costs.jl
+++ src/simulations/costs.jl
@@ -1,12 +1,28 @@
 ######################################################################
 # costs.jl
+#
+# TODO: export CostModel so people can extend it?
 ######################################################################
-export ConstantCost, MoveAndRotateCost
+export ConstantCost, MoveCost, MoveCost2, MoveAndRotateCost
 
 abstract CostModel
 
 type ConstantCost <: CostModel
 	value::Float64
+end
+
+type MoveCost <: CostModel
+	speed::Float64
+
+	MoveCost(speed::Real) = new(float(speed))
+	MoveCost() = new(1.0)
+end
+
+type MoveCost2 <: CostModel
+	speed::Float64
+
+	MoveCost2(speed::Real) = new(float(speed))
+	MoveCost2() = new(1.0)
 end
 
 type MoveAndRotateCost <: CostModel
@@ -20,6 +36,20 @@ end
 
 function get_action_cost(a::Action, cc::ConstantCost)
 	return cc.value
+end
+
+function get_action_cost(a::Action, mc::MoveCost)
+	dx = a[1]
+	dy = a[2]
+	dist = sqrt(dx*dx + dy*dy)
+	return (dist / mc.speed)
+end
+
+# TODO: should I incorporate speed somehow?
+function get_action_cost(a::Action, mc::MoveCost2)
+	dx = a[1]
+	dy = a[2]
+	return dx*dx + dy*dy
 end
 
 function get_action_cost(a::Action, marc::MoveAndRotateCost)
