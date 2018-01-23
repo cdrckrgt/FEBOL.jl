6026ea78171e238565dca6c533453743e3b64464
dressel <louisdressel@gmail.com>
2018-01-22 16:53:13 -0800

    formatting indents

--- src/simulations/simunit.jl
+++ src/simulations/simunit.jl
@@ -14,65 +14,65 @@ type `SimUnit` has fields
 Constructor: `SimUnit(x, f, p, cm)`
 """
 type SimUnit
-	x::Vehicle
-	f::AbstractFilter
-	p::Policy
-	cm::CostModel
+    x::Vehicle
+    f::AbstractFilter
+    p::Policy
+    cm::CostModel
 
-	function SimUnit(x::Vehicle, f::AbstractFilter, p::Policy, cm::CostModel=ConstantCost(1.0))
-		return new(x,f,p,cm)
-	end
+    function SimUnit(x::Vehicle, f::AbstractFilter, p::Policy, cm::CostModel=ConstantCost(1.0))
+        return new(x,f,p,cm)
+    end
 end
 
 function simulate(m::SearchDomain, uav::SimUnit, tc::TerminationCondition; video::Bool=false, pause_time=0.3)
 
-	# reset the filter, vehicle, and policy
-	# TODO: I think I assume the SimUnit comes in clean and ready to go
-	#reset!(uav.f)
-	#reset!(m, uav.x)
-	#reset!(uav.p)
+    # reset the filter, vehicle, and policy
+    # TODO: I think I assume the SimUnit comes in clean and ready to go
+    #reset!(uav.f)
+    #reset!(m, uav.x)
+    #reset!(uav.p)
 
-	# before doing anything else, we observe
-	#  and update filter once
-	o = observe(m, uav.x)
-	update!(uav, o)
+    # before doing anything else, we observe
+    #  and update filter once
+    o = observe(m, uav.x)
+    update!(uav, o)
 
-	# This was our first step; steps count number of observations
-	step_count = 1
+    # This was our first step; steps count number of observations
+    step_count = 1
 
-	# plot if need be
-	if video
-		figure("Simulation")
-		plot(m, uav.f, uav.x)
-		title("i = $(step_count)")
-	end
+    # plot if need be
+    if video
+        figure("Simulation")
+        plot(m, uav.f, uav.x)
+        title("i = $(step_count)")
+    end
 
-	# What was the cost to getting this first observation?
-	temp_cost = get_action_cost(uav.cm)
+    # What was the cost to getting this first observation?
+    temp_cost = get_action_cost(uav.cm)
 
-	while !is_complete(uav.f, tc, step_count)
-		# act
-		a = action(m, uav, o)
-		act!(m, uav.x, a)
+    while !is_complete(uav.f, tc, step_count)
+        # act
+        a = action(m, uav, o)
+        act!(m, uav.x, a)
 
-		# get cost and update step count
-		temp_cost += get_action_cost(a, uav.cm)
-		step_count += 1
+        # get cost and update step count
+        temp_cost += get_action_cost(a, uav.cm)
+        step_count += 1
 
-		# observe and update
-		o = observe(m, uav.x)
-		update!(uav, o)
+        # observe and update
+        o = observe(m, uav.x)
+        update!(uav, o)
 
-		# plot if need be
-		if video
-			pause(pause_time)
-			figure("Simulation")
-			cla()
-			plot(m, uav.f, uav.x)
-			max_b = maximum(uav.f.b)
-			title("i = $(step_count), max = $(round(max_b,3))")
-		end
-	end
+        # plot if need be
+        if video
+            pause(pause_time)
+            figure("Simulation")
+            cla()
+            plot(m, uav.f, uav.x)
+            max_b = maximum(uav.f.b)
+            title("i = $(step_count), max = $(round(max_b,3))")
+        end
+    end
 
-	return temp_cost
+    return temp_cost
 end
