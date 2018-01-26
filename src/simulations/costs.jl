ae03bd94a09745553dd482354165cf6c2d094490
dressel <louisdressel@gmail.com>
2018-01-26 11:18:04 -0800

    major restructuring to measuring simulation costs

--- src/simulations/costs.jl
+++ src/simulations/costs.jl
@@ -6,60 +6,60 @@ export CostModel, ConstantCost, MoveCost, MoveCost2, MoveAndRotateCost
 
 abstract type CostModel end
 
-type ConstantCost <: CostModel
-	value::Float64
+struct ConstantCost <: CostModel
+    value::Float64
 end
 
-type MoveCost <: CostModel
-	speed::Float64
+struct MoveCost <: CostModel
+    speed::Float64
 
-	MoveCost(speed::Real) = new(float(speed))
-	MoveCost() = new(1.0)
+    MoveCost(speed::Real) = new(float(speed))
+    MoveCost() = new(1.0)
 end
 
-type MoveCost2 <: CostModel
-	speed::Float64
+struct MoveCost2 <: CostModel
+    speed::Float64
 
-	MoveCost2(speed::Real) = new(float(speed))
-	MoveCost2() = new(1.0)
+    MoveCost2(speed::Real) = new(float(speed))
+    MoveCost2() = new(1.0)
 end
 
-type MoveAndRotateCost <: CostModel
-	speed::Float64
-	time_per_rotation::Float64
+struct MoveAndRotateCost <: CostModel
+    speed::Float64
+    time_per_rotation::Float64
 end
 
-function get_action_cost(a::Action, cm::CostModel)
-	error("get_action_cost not implemented for this cost model type.")
+function get_cost(cm::CostModel, f::AbstractFilter, a::Action)
+    error("get_cost not implemented for this cost model type.")
 end
-function get_action_cost(cm::CostModel)
-	error("get_action_cost not implemented for this cost model type.")
+function get_cost(cm::CostModel, f::AbstractFilter)
+    error("get_cost not implemented for this cost model type.")
 end
 
 
-function get_action_cost(a::Action, cc::ConstantCost)
-	return cc.value
+function get_cost(cc::ConstantCost, f::AbstractFilter, a::Action)
+    return cc.value
 end
-get_action_cost(cc::ConstantCost) = cc.value
+get_cost(cc::ConstantCost, f::AbstractFilter) = cc.value
 
-function get_action_cost(a::Action, mc::MoveCost)
-	dx = a[1]
-	dy = a[2]
-	dist = sqrt(dx*dx + dy*dy)
-	return (dist / mc.speed)
+function get_cost(mc::MoveCost, f::AbstractFilter, a::Action)
+    dx = a[1]
+    dy = a[2]
+    dist = sqrt(dx*dx + dy*dy)
+    return (dist / mc.speed)
 end
 
 # TODO: should I incorporate speed somehow?
-function get_action_cost(a::Action, mc::MoveCost2)
-	dx = a[1]
-	dy = a[2]
-	return dx*dx + dy*dy
+function get_cost(mc::MoveCost2, f::AbstractFilter, a::Action)
+    dx = a[1]
+    dy = a[2]
+    return dx*dx + dy*dy
 end
 
-function get_action_cost(a::Action, marc::MoveAndRotateCost)
-	dx = a[1]
-	dy = a[2]
-	dist = sqrt(dx*dx + dy*dy)
-	return (dist / marc.speed) + marc.time_per_rotation
+function get_cost(marc::MoveAndRotateCost, f::AbstractFilter, a::Action)
+    dx = a[1]
+    dy = a[2]
+    dist = sqrt(dx*dx + dy*dy)
+    return (dist / marc.speed) + marc.time_per_rotation
 end
-get_action_cost(marc::MoveAndRotateCost) = marc.time_per_rotation
+get_cost(marc::MoveAndRotateCost, f::AbstractFilter) = marc.time_per_rotation
