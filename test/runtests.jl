ab99d88863782c17cd2554eaf012dcb8500c9872
dressel <louisdressel@gmail.com>
2018-01-29 14:59:17 -0800

    more substantial testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,21 @@ m = SearchDomain(400, 170, 210)
 m = SearchDomain(400)
 theta!(m)
 theta!(m, 32, 40)
+
+# BearingOnly
+s = BearingOnly()
+@test s.noise_sigma == 10.0
+s = BearingOnly(5)
+@test s.noise_sigma == 5.0
+
+# a trial simulation
+m = SearchDomain(200)
+s = FOV()
+f = DF(m, 41, s, 0:1)
+x = Vehicle(100, 100, 0, 5, s)
+p = GreedyPolicy(x, 4)
+simulate(m, x, f, p)
+
+# simunit way
+tc = StepThreshold(10)
+su = SimUnit(x, f, p, tc)
