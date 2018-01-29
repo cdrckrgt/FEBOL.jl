1045389148dcf05d5bf215a0358830ab8dd81638
dressel <louisdressel@gmail.com>
2018-01-29 13:51:26 -0800

    improved comments for sensor functions

--- src/sensors/sensors.jl
+++ src/sensors/sensors.jl
@@ -4,19 +4,21 @@
 
 abstract type Sensor end
 
+# Returns a possible observation to be received by sensor s from pose p
+#  assuming the target is located according to m
+function observe(m::SearchDomain, s::Sensor, p::Pose)
+    error("`observe` not defined for this sensor.")
+end
+
+# Returns probability of measuring o with sensor s if vehicle pose is p
+#  and target location is theta
+function O(s::Sensor, theta::LocTuple, p::Pose, o)
+    error("`O` (density version) not defined for this sensor.")
+end
+
 include("bearing.jl")
 include("range.jl")
 include("diromni.jl")
 include("fov.jl")
 include("fovn.jl")
 include("fov3.jl")
-
-function observe(m::SearchDomain, s::Sensor, p::Pose)
-    error("`observe` not defined for this sensor.")
-end
-
-# Required for particle filter
-# does not require probability, returns density
-function O(s::Sensor, theta::LocTuple, p::Pose, o)
-    error("`O` (density version) not defined for this sensor.")
-end
